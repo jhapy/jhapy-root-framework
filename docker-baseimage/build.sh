017bfd1610d535d62b6b6b80dc655d143db41bb1
jHapy Lead Dev <jhapy@jhapy.org>
2020-10-18 13:11:53 +0200

    K8S CI

--- docker-baseimage/build.sh
+++ docker-baseimage/build.sh
@@ -5,7 +5,16 @@ docker build -f DockerfileSlim -t jhapy/base-image-slim:1.0 .
 docker build -f DockerfileSlimWithTesseract -t jhapy/base-image-slim-tesseract:1.0 .
 docker build -f DockerfileSlimWithOpenoffice -t jhapy/base-image-slim-ooffice:1.0 .
 
+docker tag jhapy/base-image:1.0 jhapy/base-image:latest
+docker tag jhapy/base-image-slim:1.0 jhapy/base-image-slim:latest
+docker tag jhapy/base-image-slim-tesseract:1.0 jhapy/base-image-slim-tesseract:latest
+docker tag jhapy/base-image-slim-ooffice:1.0 jhapy/base-image-slim-ooffice:latest
+
 docker push jhapy/base-image:1.0
+docker push jhapy/base-image:latest
 docker push jhapy/base-image-slim:1.0
+docker push jhapy/base-image-slim:latest
+docker push jhapy/base-image-slim-tesseract:1.0
+docker push jhapy/base-image-slim-tesseract:latest
 docker push jhapy/base-image-slim-ooffice:1.0
-docker push jhapy/base-image-slim-jdkoracle:1.0
+docker push jhapy/base-image-slim-ooffice:latest
